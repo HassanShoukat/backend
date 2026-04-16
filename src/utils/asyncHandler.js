bae98fdda6e2ad57f5328cc404a7785b922e00bb
HassanShoukat <hassanshoukat485@gmail.com>
2026-04-16 19:40:36 +0500

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch(next);
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = () => {};
+// const asynchandler = (func) => () => {};
+// const asynchandler = (func) => async () => {};
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };

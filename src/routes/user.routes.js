345d229e4a631576b980420b644d5f765e9f1b9c
HassanShoukat <hassanshoukat485@gmail.com>
2026-04-21 18:18:28 +0500

    middleware login and logout controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -8,5 +13,8 @@ router.route("/register").post(
   upload.any(), // Accept any files - we'll validate in controller
   registerUser
 );
+router.route("/login").post(loginUser);
 
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
 export default router;
